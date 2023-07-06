882909831ad9e27b7bb8f37247d0ba9ee9c5ee49
Christian <97773844+esuriru@users.noreply.github.com>
2023-07-06 14:10:13 +0800

    d

---
+++ Application/Source/EC/Components/Rigidbody2D.cpp
@@ -0,0 +1,7 @@
+#include "Rigidbody2D.h"
+
+Rigidbody2D::Rigidbody2D(GameObject &gameObject)
+    : Component(gameObject)
+{
+
+}
---
+++ Application/Source/EC/Components/Rigidbody2D.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "EC/Component.h"
+
+class Rigidbody2D : public Component
+{
+public:
+    Rigidbody2D(GameObject& gameObject);
+
+};
